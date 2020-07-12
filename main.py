653103f65d11e26ff2448e12ebe4971171381613
Protivofazza <protivofazza@outlook.com>
2020-07-12 20:22:39 +0300

    pre deploy

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 from web_shop.bot.bot_main import app, set_webhook, bot
 from web_shop.db.seeder import seed, generate
-from web_shop.bot.config import DEBUG
+from web_shop.bot import config
 from web_shop.db.models import is_db_empty
 from web_shop.api.api_main import start as startAPI
 from web_shop.log_writer import log_write, log_clear
 
+from flask import Flask, request, abort
+from telebot.types import Update
 
-log_write("\n\nRESTARTING BOT..\n\n")
-log_write("STARTING LOGGING..")
-if is_db_empty():
-    generate(category=20, product=46, news=8)
-if not DEBUG:
-    log_write("STARTING API")
-    startAPI()
-    log_write("API STARTED SUCCESSFULLY")
-    set_webhook()
-    log_write("WEBHOOK SET SUCCESSFULLY")
-    log_write("STARTING SERVER")
-    app.run(port=8000)
-else:
-    bot.polling()
-    startAPI()
-    log_write("API STARTED SUCCESSFULLY")
-    app.run(port=8000)
+
+app = Flask(__name__)
+
+
+@app.route(config.WEBHOOK_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') =='application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(status=403)
+
+
+if __name__ == '__main__':
+    bot.set_webhook(config.WEBHOOK_URL,
+                    certificate=open('webhook_cert.pem', 'r')
+                    )
+
+
+# log_write("\n\nRESTARTING BOT..\n\n")
+# log_write("STARTING LOGGING..")
+# if is_db_empty():
+#     generate(category=20, product=46, news=8)
+# if not DEBUG:
+#     log_write("STARTING API")
+#     startAPI()
+#     log_write("API STARTED SUCCESSFULLY")
+#     set_webhook()
+#     log_write("WEBHOOK SET SUCCESSFULLY")
+#     log_write("STARTING SERVER")
+#     app.run(port=8000)
+# else:
+#     bot.polling()
+#     startAPI()
+#     log_write("API STARTED SUCCESSFULLY")
+#     app.run(port=8000)
 
 
 
--- web_shop/bot/config.py
+++ web_shop/bot/config.py
@@ -1,5 +1,6 @@
 TOKEN = '1214756976:AAHB8LKl3IdEPz_6HNEa57SVajt7-H_RuRo'  # Мій токен №3 від 10.07
+GLOBAL_IP = '34.76.230.66'
+WEBHOOK_PATH = '/tg'
+WEBHOOK_URL = f'https://{GLOBAL_IP}{WEBHOOK_PATH}'
+
 DEBUG = True  # Змінити на False при розміщенні
-# GLOBAL_IP = '34.78.198.174'
-# WEBHOOK_PATH = '/tg'
-# WEBHOOK_URL = f'https://{GLOBAL_IP}{WEBHOOK_PATH}'
