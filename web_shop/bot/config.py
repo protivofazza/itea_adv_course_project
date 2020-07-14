ab5211c2ae830ca354708ca2af412c1f7eb1ab7d
Protivofazza <protivofazza@outlook.com>
2020-07-14 08:14:17 +0300

    new comments in config.py

--- web_shop/bot/config.py
+++ web_shop/bot/config.py
@@ -1,7 +1,7 @@
 
-TOKEN = '1214756976:AAGIThZ1Gp43F6eVwLvybTOAPxP1M7Ac3VU'  # Мій токен №3 від 10.07
+TOKEN = '1214756976:AAGIThZ1Gp43F6eVwLvybTOAPxP1M7Ac3VU'  # Мій токен для ВМ, отриманий через /revoke 13.07.2020
 GLOBAL_IP = '34.76.230.66'
 WEBHOOK_PATH = '/tg'
 WEBHOOK_URL = f'https://{GLOBAL_IP}{WEBHOOK_PATH}'
 
-DEBUG = False  # Змінити на False при розміщенні
+DEBUG = False  # Змінюється на False при розміщенні на ВМ
