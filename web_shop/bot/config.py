5bdd6b2c4bbd5098c7958f6a8ce3e217e422cc1a
Protivofazza <protivofazza@outlook.com>
2020-07-14 15:18:59 +0300

    delete comments in main.py

--- web_shop/bot/config.py
+++ web_shop/bot/config.py
@@ -1,7 +1,7 @@
 
-TOKEN = '1214756976:AAGIThZ1Gp43F6eVwLvybTOAPxP1M7Ac3VU'  # Мій токен для ВМ, отриманий через /revoke 13.07.2020
+TOKEN = '1126968508:AAE21gNHmtIBjIRnUls99JaPd8W_LCss8uM'  # Токен №5 від 13.07 03:59 для бота t.me/edu_itea_web_shop_bot
 GLOBAL_IP = '34.76.230.66'
 WEBHOOK_PATH = '/tg'
 WEBHOOK_URL = f'https://{GLOBAL_IP}{WEBHOOK_PATH}'
 
-DEBUG = False  # Змінюється на False при розміщенні на ВМ
+DEBUG = True  # Змінюється на False при розміщенні на ВМ
